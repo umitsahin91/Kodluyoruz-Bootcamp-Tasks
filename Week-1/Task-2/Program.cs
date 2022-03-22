05de610a0b47484761ddb7af366fb3f6e636d2c4
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-22 23:42:47 +0300

    Added task 2

---
+++ Week-1/Task-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /* 
+             * Program that detects that the entered number is a prime number
+             */
+            
+            try 
+	        {	        
+		        int num;
+                int counter=0;
+                Console.WriteLine("Enter a number : ");
+                number = int.Parse(Console.ReadLine());
+                int i=2;
+
+                while (i<num)
+                {
+                    if (num % i == 0)
+                    {
+                    counter++;
+                    }
+
+                    i++;
+                }
+
+                if (counter != 0)
+                {
+                Console.WriteLine("Number is NOT Prime");
+                }
+                else
+                {
+                Console.WriteLine("Number is PRIME");
+                }
+	        }
+	        
+            catch (FormatException)
+	        {
+                Console.WriteLine("Please enter numbers only");
+	        }
+
+            Console.ReadLine();
+        }
+    }
+}
