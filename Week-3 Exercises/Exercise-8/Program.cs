69d52a4e6788ab9d6ffe3a7d78947f50b93b17c0
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 16:17:35 +0300

    Added Exercise-8

---
+++ Week-3 Exercises/Exercise-8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Girilen string ifadede boşluk karakterine kadar olan kısmı yazdıran
+            program.(Örnek: Kodluyoruz’da yer alan bootcamp’lere başvuracağım cümlesi
+            girildiğinde ekrana sadece Kodluyoruz’da yazdırılması gerekir.)
+            */
+            string text="Kodluyoruz’da yer alan bootcamp’lere başvuracağım";
+            int index=text.IndexOf(" ");
+            Console.Write(text.Substring(0,index));
+
+        }
+    }
+}
