6a029703002faac11e03136143179c9203f98cd5
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 16:25:18 +0300

    Added Exercise-1

---
+++ Week-3 Exercises/Exercise-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Klavyeden girilen metnin kaç kelimeden oluştuğunu bulan program.
+            */
+            WordCounter wc = new WordCounter();
+            Console.Write("Lütfen metni giriniz : ");
+            string[] text = wc.Split(Console.ReadLine());
+
+            Console.WriteLine(wc.Counter(text));
+        }
+    }
+    class WordCounter
+    {
+        /*
+        Girilen metni split metotu ile string diziye dönüştüren metot.
+        */
+        public string[] Split(string text)
+        {
+
+            string[] _text = text.Split(' ');
+            return _text;
+        }
+        /*
+        Gönderilen dizinin içerisinde ki boşlukların kontrolü yapılıp
+        geriye kelime sayısı döndürülüyor.
+        */
+        public int Counter(string[] text)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == "")
+                {
+                    counter++;
+                }
+            }
+
+            return text.Length - counter;
+        }
+
+    }
+}
