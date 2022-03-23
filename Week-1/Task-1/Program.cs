a746f667185c3a383899d17d6abe38932a1e7b19
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 08:59:18 +0300

    Added task 1

--- Week-1/Task-1/Program.cs
+++ Week-1/Task-1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Task_1
 {
@@ -9,13 +9,14 @@ namespace Task_1
             /*
             Program that calculates the area and perimeter of a rectangle and displays it on the screen.
             */
-           System.Console.Write("Short Ledge : ");
-           int sorthLedge=int.Parse(Console.ReadLine());
-           System.Console.Write("Long Ledge : ");
-           int longLedge=int.Parse(Console.ReadLine());
+            
+           Console.Write("Short Edge : ");
+           int sorthEdge=int.Parse(Console.ReadLine());
+           Console.Write("Long Edge : ");
+           int longEdge=int.Parse(Console.ReadLine());
 
-           System.Console.WriteLine($"Rectangel Contour : {(sorthLedge*2)+(longLedge*2)}");
-           System.Console.WriteLine($"Rectangel Area : {sorthLedge*longLedge}");
+           Console.WriteLine($"Rectangel Contour : {(sorthEdge*2)+(longEdge*2)}");
+           Console.WriteLine($"Rectangel Area : {sorthEdge*longEdge}");
         }
     }
 }
