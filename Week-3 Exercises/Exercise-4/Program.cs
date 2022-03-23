79a2e968d2ca4f4237883739c8326df4f9d740d0
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 16:51:24 +0300

    Added Exercise-4

---
+++ Week-3 Exercises/Exercise-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Klavyeden girilen bir sayının faktöriyelini alan program.
+            */
+            Console.Write("Bir sayı gitin : ");
+            int fact=int.Parse(Console.ReadLine());
+            Console.Write($"Girmiş olduğunuz sayının faktöriyeli : {Factorial(fact)}");
+        }
+        /*
+        Faktöriyel hesaplayan rekürsif metot.
+        */
+         static int Factorial(int number)
+        {
+            if (number==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number*Factorial(number-1);
+            }
+        }
+    }
+}
