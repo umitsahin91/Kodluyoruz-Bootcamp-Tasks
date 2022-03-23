1b2883d1fe33bf6efc345818739f0c7d934e9d01
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 08:54:13 +0300

    Added task 3

---
+++ Week-1/Task-3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Program listing prime numbers between 1-10000
+            */
+            
+            int num = 2;
+            int counter=0;
+
+            for (int i = 2; i < 10000; i++)
+            {
+                while (num < i)
+                {
+                    if (i % num == 0)
+                        counter++;
+
+                    num++;
+                }
+                
+                if (counter==0)
+                {
+                    Console.Write(i+" ");
+                }
+                counter=0;
+                num=2;
+            }
+        }
+    }
+}
