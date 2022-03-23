f2265dc6adca13c435e58bffbe65bacf1e68aba0
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-23 16:09:30 +0300

    Added Exercise-7

---
+++ Week-3 Exercises/Exercise-7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+            Derece olarak verilen sıcaklığı Fahrenheita çeviren program
+            */
+            Console.Write("Derece değerini girin  : ");
+            int celcius=int.Parse(Console.ReadLine());
+            int fahrenheit=(celcius*9/5)+32;
+            Console.Write($"Girmiş olduğunuz derecenin Fahrenheit karşılığı : {fahrenheit}");
+    }
+}
