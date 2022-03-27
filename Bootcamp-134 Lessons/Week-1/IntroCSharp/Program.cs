7bbf65f0b74e8ab6c9ef6103e2d42f90b11c19e7
Umit Sahin <umit.sahin91@hotmail.com>
2022-03-27 18:12:33 +0300

    Added file

---
+++ Bootcamp-134 Lessons/Week-1/IntroCSharp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace IntroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sayısal veri tipleri
+            byte age = 255;
+            sbyte negativeNumber = -4;
+
+            short length = -32768;
+
+            ushort unsignedShort = 65535;
+
+            int totalYear = 1000000000;
+
+
+            long watchCount = 100000000000;
+
+            double pi = 3.14;
+            float discount = 0.15f;
+            decimal formulaConstant = 0.000000000000000001M;
+
+
+
+            //sözel
+            char character = 'p';
+            string word = "Türkay Ürkmez";
+
+
+
+            //mantıksal
+            bool isClosed = true;
+
+            /*
+             * Amaç: Kullanıcının boy ve kilosuna göre BMI hesaplamak
+             */
+            try
+            {
+                Console.WriteLine("Your weight:");
+                string weightValue = Console.ReadLine();
+                int weight = Convert.ToInt32(weightValue);
+
+                Console.WriteLine("Your tall (meter unit): ");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                double bmi = weight / (height * height);
+
+                Console.WriteLine("Your bmi value is " + bmi);
+
+                if (bmi < 18.5)
+                {
+                    Console.WriteLine("You are Underweight");
+                }
+                else if (bmi <= 24.9)
+                {
+                    Console.WriteLine("You are normal weight");
+                }
+                else if (bmi <= 29.9)
+                {
+                    Console.WriteLine("You are overweight");
+                }
+                else
+                {
+                    Console.WriteLine("You are obese");
+
+                }
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please give values with true format");
+            }
+        }
+    }
+}
