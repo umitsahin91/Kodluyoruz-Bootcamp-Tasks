e15cd6683107421c4a71394951a2940d39f8ef77
Umit Sahin <umit.sahin91@hotmail.com>
2022-04-08 00:18:13 +0300

    Week-2 Task-1 added

---
+++ Bootcamp-134 Homework/Week-2/Task-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          List<string> cities= new List<string>{"Ankara","Trabzon","Bursa","Adana","Kastamonu","Ankara","Trabzon","Bursa","Adana","Kastamonu"};
+          List<string> distinctCities= new List<string>();
+          foreach (var city in cities)
+          {
+              if (!distinctCities.Contains(city))
+              {
+                  distinctCities.Add(city);
+              }    
+          }      
+        }
+    }
+}
